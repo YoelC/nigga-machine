704c4b78b7a2c19fc7118d8e315bcc1c3a7b4370
YoelC <yoelcoriat26@gmail.com>
2020-02-13 22:01:07 -0500

    Added Nigga Machine Sword and Nigga Machine Stick (plus crafting)

--- src/main/java/com/yoelc/firstmod/init/ModItems.java
+++ src/main/java/com/yoelc/firstmod/init/ModItems.java
@@ -1,8 +1,10 @@
 package com.yoelc.firstmod.init;
 
 import com.yoelc.firstmod.items.ItemBase;
+import com.yoelc.firstmod.items.tools.ToolSword;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemSword;
 import net.minecraftforge.common.util.EnumHelper;
 
 import java.util.ArrayList;
@@ -11,8 +13,15 @@ import java.util.List;
 public class ModItems {
     public static final List<Item> ITEMS = new ArrayList<Item>();
 
-    // Blocks
+    // Materials
+    public static final Item.ToolMaterial MATERIAL_NIGGA_MACHINE = EnumHelper.addToolMaterial("material_nigga_machine", 3, 3000, 10.0f, 5.0f, 50);
+
+    // Items
     public static final Item NIGGA_MACHINE = new ItemBase("nigga_machine");
+    public static final Item NIGGA_MACHINE_STICK = new ItemBase("nigga_machine_stick");
+
+    // Tools
+    public static final ItemSword NIGGA_MACHINE_SWORD = new ToolSword("nigga_machine_sword", MATERIAL_NIGGA_MACHINE);
 
 
 }
---
+++ src/main/java/com/yoelc/firstmod/items/tools/ToolSword.java
@@ -0,0 +1,23 @@
+package com.yoelc.firstmod.items.tools;
+
+import com.yoelc.firstmod.Main;
+import com.yoelc.firstmod.init.ModItems;
+import com.yoelc.firstmod.util.IHasModel;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemSword;
+
+public class ToolSword extends ItemSword implements IHasModel {
+
+    public ToolSword(String name, ToolMaterial material)  {
+        super(material);
+        setUnlocalizedName(name);
+        setRegistryName(name);
+        setCreativeTab(CreativeTabs.COMBAT);
+        ModItems.ITEMS.add(this);
+    }
+
+    @Override
+    public void registerModels() {
+        Main.proxy.registerItemRenderer(this, 0, "inventory");
+    }
+}
