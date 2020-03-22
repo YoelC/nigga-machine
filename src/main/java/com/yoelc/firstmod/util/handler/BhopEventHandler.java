7db5fd885e647b706a15e41cfa3a4edb0eafa061
YoelC <yoelcoriat26@gmail.com>
2020-03-21 23:46:20 -0500

    Progress on better code

--- src/main/java/com/yoelc/firstmod/util/handler/BhopEventHandler.java
+++ src/main/java/com/yoelc/firstmod/util/handler/BhopEventHandler.java
@@ -11,25 +11,17 @@ import static java.lang.Math.max;
 
 public class BhopEventHandler {
 
-    public boolean onGroundLastTick = false;
-    public float airAccel = 1f;
-    public float airMax = 0.01f;
-    public float groundMax = 0.03f;
-    public float friction = 0.4f;
+    public float airAccel = 0.15f;
+    public float airMax = 0.003f;
+    public float groundMax = 3f;
+    public float friction = 0.6f;
     public float groundAccel = 0.13f;
 
     public Vector3D vel = new Vector3D();
     public Vector3D prevVel = new Vector3D();
-    public boolean jumping = false;
 
     @SubscribeEvent
     public void onPlayerTick(TickEvent.PlayerTickEvent event) {
-
-        // Auto Bhop
-        jumping = Keyboard.isKeyDown(Keyboard.KEY_SPACE);
-        if (jumping && event.player.onGround)  {
-            event.player.jump();
-        }
 
         if (event.phase == TickEvent.Phase.START && event.side == Side.CLIENT) {
             vel.x = prevVel.x;
@@ -37,41 +29,21 @@ public class BhopEventHandler {
 
             vel = calculateFriction(event);
 
-            if (getOnGround(event)) {
+            if (event.player.onGround) {
                 vel = vel.add(moveGround(event));
             } else {
                 vel = vel.add(moveAir(event));
             }
 
-            event.player.motionX = vel.x / 5;
-            event.player.motionZ = vel.z / 5;
+            event.player.motionX = vel.x;
+            event.player.motionZ = vel.z;
             prevVel.x = vel.x;
             prevVel.z = vel.z;
         }
     }
 
-    public boolean getOnGround(TickEvent.PlayerTickEvent event) {
-        if(event.player.onGround && !onGroundLastTick) {
-            onGroundLastTick = true;
-            return false;
-
-        }
-
-        else if(event.player.onGround && onGroundLastTick) {
-            return true;
-        }
-
-        else if(!event.player.onGround) {
-            onGroundLastTick = false;
-            return false;
-
-        }
-
-        return false;
-    }
-
     public Vector3D calculateFriction(TickEvent.PlayerTickEvent event) {
-        if (getOnGround(event)) {
+        if (event.player.onGround) {
             vel = vel.scale(friction);
         }
         return vel;
@@ -87,12 +59,12 @@ public class BhopEventHandler {
     }
 
     public Vector3D accelerate(TickEvent.PlayerTickEvent event, float accelerate, float maxVel) {
-        Vector3D wishvel = getWishvel(event);
+        Vector3D wishvel = getWishvel(event).scale(accelerate);
         float max = Math.max(0, 1 - vel.length() / maxVel);
-        System.out.println(max);
         float projVel = vel.dot(wishvel);
         Vector3D scaledWishvel = wishvel.scale(max);
-        return wishvel.lerp(scaledWishvel, projVel);
+        Vector3D lerp = wishvel.lerp(scaledWishvel, 1 / projVel);
+        return lerp;
     }
 
     public Vector3D moveAir(TickEvent.PlayerTickEvent event) {
